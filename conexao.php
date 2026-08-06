6018ea868bce22fd424900c6096b800e1567cc31
mhfabri <matheusfabri331@gmail.com>
2026-08-06 11:47:32 -0300

    aula 02 - conexao com banco primeira pagina

--- conexao.php
+++ conexao.php
@@ -1,5 +1,16 @@
 <?php
+    // conexao.php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "sistema_produtos";
 
 
+    $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
+    if ($conexao){
+        echo ("conectado");
+    }else{
+        echo ("ao conectar: ". mysqli_connect_error());
+    }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php
-
-
-
-?>
+<?php include 'cabecalho.php'; ?>
+    <main>
+        <p>Bem-vindo(a) ao sistema!</p>
+        <p>Hoje é <?php echo date('d/m/Y'); ?></p>
+    </main>
+<?php include 'rodape.php'; ?>
